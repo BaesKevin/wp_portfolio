5081b0800714ca980e1aeec809088f5e7e9f8ca8
BaesKevin <kevinbaes@hotmail.be>
2018-11-28 11:16:38 +0100

    Create portfolio item custom post type.
    Post type has title, project url and a thumbnail.

--- functions.php
+++ functions.php
@@ -124,9 +124,88 @@ function jobexperience_save_data(){
             update_post_meta($post->ID, JOBEXPERIENCE_DESCRIPTION, $_POST[JOBEXPERIENCE_DESCRIPTION]);
         }    
     }
-   
 }
 
 add_action('init', 'jobexperience_init');
 add_action('admin_init', 'jobexperience_admin_init');
 add_action('save_post', 'jobexperience_save_data');
+
+/*****************************************
+PORTFOLIO ITEM CUSTOM POST TYPE
+******************************************/
+
+// define constants for jobexperience post type and field names
+define("PORTFOLIO_ITEM_POSTTYPE", "portfolio_item");
+define("PORTFOLIO_ITEM_URL", "portfolio_item_url");
+
+// register the post type that displays experience entries on the front page
+function portfolio_item_init(){
+    $labels = array(
+    	'name' => __('Portfolio items'),
+    	'singular_name' => __('Portfolio item'),
+    	'add_new_item' => __('Add new portfolio item with a thumbnail, a name and a link to the project.')
+    );
+    $args = array(
+    	'labels' => $labels,
+    	'description' => 'Holds links to all your projects',
+    	'public' => true,
+    	'has_archive' => true,
+    	'supports' => array('title', 'thumbnail')
+    );
+    
+    register_post_type(PORTFOLIO_ITEM_POSTTYPE, $args);
+}
+
+function portfolio_item_admin_init(){
+    // add a box to add custom fields to
+    add_meta_box(
+    	'portfolio_item_details', //id
+        'Portfolio Item Details', //title
+        'portfolio_item_callback', //callback function
+        PORTFOLIO_ITEM_POSTTYPE, // 'post type' to add the metabox to
+        'normal', //'context'
+        'high' //priority
+    );
+
+    // define custom fields for metabox
+    function portfolio_item_callback(){
+        global $post; //pull in the global post variable
+        $custom = get_post_custom($post->ID);
+        $port_item_url = '';
+
+        if(isset($custom[PORTFOLIO_ITEM_URL])){
+            $port_item_url = $custom[PORTFOLIO_ITEM_URL][0];
+        }
+        
+        // echo html for this custom metabox, wordpresss makes a form around this
+        ?>
+         Please enter the project details.
+        
+        <div>
+            <label for="<?php echo PORTFOLIO_ITEM_URL; ?>">Project URL</label>
+            <input id="<?php echo PORTFOLIO_ITEM_URL; ?>"
+                   type="url"
+                   name="<?php echo PORTFOLIO_ITEM_URL; ?>" value="<?php echo $port_item_url; ?>"/>
+        </div>
+            
+        <?php
+            
+        
+    }
+}
+
+// CAUTION this gets called when saving ANY post type, so we have to use isset to make sure saving regular posts does not break
+// isset($post) must be used because wordpress thinks it's logical to call this on page load
+function portfolio_item_save_data(){
+    global $post; 
+
+    if(isset($post)){
+        if(isset($_POST[PORTFOLIO_ITEM_URL])){
+            update_post_meta($post->ID, PORTFOLIO_ITEM_URL, $_POST[PORTFOLIO_ITEM_URL]);
+        }    
+    }
+}
+
+add_action('init', 'portfolio_item_init');
+add_action('admin_init', 'portfolio_item_admin_init');
+add_action('save_post', 'portfolio_item_save_data');
--- page-portfolio.php
+++ page-portfolio.php
@@ -1,161 +1,200 @@
 <?php get_header(); ?>
-	<div class="main-container">
-	<section class="no-pad-bottom projects-gallery">
-			<div class="projects-wrapper clearfix">
-				<div class="container">
-					<div class="projects-container column-projects">
-				
-						<div class="col-md-4 col-sm-6 project development image-holder">
-							<div class="background-image-holder">
-								<img class="background-image" alt="Background Image" src="img/project_1.jpg">
-							</div>
-							<div class="hover-state">
-								<div class="align-vertical">
-									<h3 class="text-white">Project #1</h3>
-									<a href="#" class="btn btn-primary btn-white">See Project</a>
-								</div>
-							</div>
-						</div>
-						
-						<div class="col-md-4 col-sm-6 project branding image-holder">
-							<div class="background-image-holder">
-								<img class="background-image" alt="Background Image" src="img/project_2.jpg">
-							</div>
-							<div class="hover-state">
-								<div class="align-vertical">
-									<h3 class="text-white">Project #2</h3>
-									<a href="#" class="btn btn-primary btn-white">See Project</a>
-								</div>
-							</div>
-						</div>
-						
-						<div class="col-md-4 col-sm-6 project print image-holder">
-							<div class="background-image-holder">
-								<img class="background-image" alt="Background Image" src="img/project_3.jpg">
-							</div>
-							<div class="hover-state">
-								<div class="align-vertical">
-									<h3 class="text-white">Project #3</h3>
-									<a href="#" class="btn btn-primary btn-white">See Project</a>
-								</div>
-							</div>
-						</div>
-						
-						<div class="col-md-4 col-sm-6 project print image-holder">
-							<div class="background-image-holder">
-								<img class="background-image" alt="Background Image" src="img/project_4.jpg">
-							</div>
-							<div class="hover-state">
-								<div class="align-vertical">
-									<h3 class="text-white">Project #4</h3>
-									<a href="#" class="btn btn-primary btn-white">See Project</a>
-								</div>
-							</div>
-						</div>
-						
-						<div class="col-md-4 col-sm-6 project branding image-holder">
-							<div class="background-image-holder">
-								<img class="background-image" alt="Background Image" src="img/project_5.jpg">
-							</div>
-							<div class="hover-state">
-								<div class="align-vertical">
-									<h3 class="text-white">Project #5</h3>
-									<a href="#" class="btn btn-primary btn-white">See Project</a>
-								</div>
-							</div>
-						</div>
-						
-						<div class="col-md-4 col-sm-6 project development image-holder">
-							<div class="background-image-holder">
-								<img class="background-image" alt="Background Image" src="img/project_6.jpg">
-							</div>
-							<div class="hover-state">
-								<div class="align-vertical">
-									<h3 class="text-white">Project #6</h3>
-									<a href="#" class="btn btn-primary btn-white">See Project</a>
-								</div>
-							</div>
-						</div><div class="col-md-4 col-sm-6 project development image-holder">
-							<div class="background-image-holder">
-								<img class="background-image" alt="Background Image" src="img/project_7.jpg">
-							</div>
-							<div class="hover-state">
-								<div class="align-vertical">
-									<h3 class="text-white">Project #7</h3>
-									<a href="#" class="btn btn-primary btn-white">See Project</a>
-								</div>
-							</div>
-						</div><div class="col-md-4 col-sm-6 project development image-holder">
-							<div class="background-image-holder">
-								<img class="background-image" alt="Background Image" src="img/project_8.jpg">
-							</div>
-							<div class="hover-state">
-								<div class="align-vertical">
-									<h3 class="text-white">Project #8</h3>
-									<a href="#" class="btn btn-primary btn-white">See Project</a>
-								</div>
-							</div>
-						</div><div class="col-md-4 col-sm-6 project development image-holder">
-							<div class="background-image-holder">
-								<img class="background-image" alt="Background Image" src="img/project_9.jpg">
-							</div>
-							<div class="hover-state">
-								<div class="align-vertical">
-									<h3 class="text-white">Project #9</h3>
-									<a href="#" class="btn btn-primary btn-white">See Project</a>
-								</div>
-							</div>
-						</div><div class="col-md-4 col-sm-6 project development image-holder">
-							<div class="background-image-holder">
-								<img class="background-image" alt="Background Image" src="img/project_10.jpg">
-							</div>
-							<div class="hover-state">
-								<div class="align-vertical">
-									<h3 class="text-white">Project #10</h3>
-									<a href="#" class="btn btn-primary btn-white">See Project</a>
-								</div>
-							</div>
-						</div><div class="col-md-4 col-sm-6 project development image-holder">
-							<div class="background-image-holder">
-								<img class="background-image" alt="Background Image" src="img/project_11.jpg">
-							</div>
-							<div class="hover-state">
-								<div class="align-vertical">
-									<h3 class="text-white">Project #11</h3>
-									<a href="#" class="btn btn-primary btn-white">See Project</a>
-								</div>
-							</div>
-						</div><div class="col-md-4 col-sm-6 project development image-holder">
-							<div class="background-image-holder">
-								<img class="background-image" alt="Background Image" src="img/project_12.jpg">
-							</div>
-							<div class="hover-state">
-								<div class="align-vertical">
-									<h3 class="text-white">Project #12</h3>
-									<a href="#" class="btn btn-primary btn-white">See Project</a>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				
-			</div>
-			
-		</section>
-		
-		<section class="pure-text-contact">
-			<div class="container">
-				<div class="row">
-					<div class="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 text-center">
-						<span class="sub alt-font">Do you like my works?</span>
-						<h1><span>So let's talk about your project!</span></h1>
-						<i class="icon icon-chat"></i>
-					</div>
-				</div>
-	
-			</div>
-		</section>
-	</div>
-	
-	<?php get_footer(); ?>
+<div class="main-container">
+    <section class="no-pad-bottom projects-gallery">
+        <div class="projects-wrapper clearfix">
+            <div class="container">
+                <div class="projects-container column-projects">
+
+                    <?php
+                    $args = array(
+                        'post_type' => 'portfolio_item',
+                        'posts_per_page' => 10
+                    );
+                    $the_query = new WP_Query($args);
+
+                    if ($the_query->have_posts()) : while ($the_query->have_posts()) : $the_query->the_post(); ?>
+                        <?php
+                        global $post;
+                        $portitem_custom_fields = get_post_custom($post->ID);
+                        ?>
+
+                        <div class="col-md-4 col-sm-6 project development image-holder">
+                            <div class="background-image-holder">
+                                <img class="background-image"
+                                     alt="Background Image"
+                                     src="<?php echo get_field("portfolio_item_thumbnail")["url"] ?>">
+                            </div>
+                            <div class="hover-state">
+                                <div class="align-vertical">
+                                    <h3 class="text-white"><?php echo $post->the_title; ?></h3>
+                                    <a href="<?php echo $portitem_custom_fields["portfolio_item_url"][0]; ?>"
+                                       class="btn btn-primary btn-white">
+                                        <?php echo $post->post_title; ?>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    <?php endwhile; else : ?>
+                        <p><?php esc_html_e('You didn\'t add any projects yet. You can start adding projects in the wordpress admin console.'); ?></p>
+                    <?php endif; ?>
+
+<!--                    <div class="col-md-4 col-sm-6 project development image-holder">-->
+<!--                        <div class="background-image-holder">-->
+<!--                            <img class="background-image" alt="Background Image" src="img/project_1.jpg">-->
+<!--                        </div>-->
+<!--                        <div class="hover-state">-->
+<!--                            <div class="align-vertical">-->
+<!--                                <h3 class="text-white">Project #1</h3>-->
+<!--                                <a href="#" class="btn btn-primary btn-white">See Project</a>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!---->
+<!--                    <div class="col-md-4 col-sm-6 project branding image-holder">-->
+<!--                        <div class="background-image-holder">-->
+<!--                            <img class="background-image" alt="Background Image" src="img/project_2.jpg">-->
+<!--                        </div>-->
+<!--                        <div class="hover-state">-->
+<!--                            <div class="align-vertical">-->
+<!--                                <h3 class="text-white">Project #2</h3>-->
+<!--                                <a href="#" class="btn btn-primary btn-white">See Project</a>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!---->
+<!--                    <div class="col-md-4 col-sm-6 project print image-holder">-->
+<!--                        <div class="background-image-holder">-->
+<!--                            <img class="background-image" alt="Background Image" src="img/project_3.jpg">-->
+<!--                        </div>-->
+<!--                        <div class="hover-state">-->
+<!--                            <div class="align-vertical">-->
+<!--                                <h3 class="text-white">Project #3</h3>-->
+<!--                                <a href="#" class="btn btn-primary btn-white">See Project</a>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!---->
+<!--                    <div class="col-md-4 col-sm-6 project print image-holder">-->
+<!--                        <div class="background-image-holder">-->
+<!--                            <img class="background-image" alt="Background Image" src="img/project_4.jpg">-->
+<!--                        </div>-->
+<!--                        <div class="hover-state">-->
+<!--                            <div class="align-vertical">-->
+<!--                                <h3 class="text-white">Project #4</h3>-->
+<!--                                <a href="#" class="btn btn-primary btn-white">See Project</a>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!---->
+<!--                    <div class="col-md-4 col-sm-6 project branding image-holder">-->
+<!--                        <div class="background-image-holder">-->
+<!--                            <img class="background-image" alt="Background Image" src="img/project_5.jpg">-->
+<!--                        </div>-->
+<!--                        <div class="hover-state">-->
+<!--                            <div class="align-vertical">-->
+<!--                                <h3 class="text-white">Project #5</h3>-->
+<!--                                <a href="#" class="btn btn-primary btn-white">See Project</a>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!---->
+<!--                    <div class="col-md-4 col-sm-6 project development image-holder">-->
+<!--                        <div class="background-image-holder">-->
+<!--                            <img class="background-image" alt="Background Image" src="img/project_6.jpg">-->
+<!--                        </div>-->
+<!--                        <div class="hover-state">-->
+<!--                            <div class="align-vertical">-->
+<!--                                <h3 class="text-white">Project #6</h3>-->
+<!--                                <a href="#" class="btn btn-primary btn-white">See Project</a>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                    <div class="col-md-4 col-sm-6 project development image-holder">-->
+<!--                        <div class="background-image-holder">-->
+<!--                            <img class="background-image" alt="Background Image" src="img/project_7.jpg">-->
+<!--                        </div>-->
+<!--                        <div class="hover-state">-->
+<!--                            <div class="align-vertical">-->
+<!--                                <h3 class="text-white">Project #7</h3>-->
+<!--                                <a href="#" class="btn btn-primary btn-white">See Project</a>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                    <div class="col-md-4 col-sm-6 project development image-holder">-->
+<!--                        <div class="background-image-holder">-->
+<!--                            <img class="background-image" alt="Background Image" src="img/project_8.jpg">-->
+<!--                        </div>-->
+<!--                        <div class="hover-state">-->
+<!--                            <div class="align-vertical">-->
+<!--                                <h3 class="text-white">Project #8</h3>-->
+<!--                                <a href="#" class="btn btn-primary btn-white">See Project</a>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                    <div class="col-md-4 col-sm-6 project development image-holder">-->
+<!--                        <div class="background-image-holder">-->
+<!--                            <img class="background-image" alt="Background Image" src="img/project_9.jpg">-->
+<!--                        </div>-->
+<!--                        <div class="hover-state">-->
+<!--                            <div class="align-vertical">-->
+<!--                                <h3 class="text-white">Project #9</h3>-->
+<!--                                <a href="#" class="btn btn-primary btn-white">See Project</a>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                    <div class="col-md-4 col-sm-6 project development image-holder">-->
+<!--                        <div class="background-image-holder">-->
+<!--                            <img class="background-image" alt="Background Image" src="img/project_10.jpg">-->
+<!--                        </div>-->
+<!--                        <div class="hover-state">-->
+<!--                            <div class="align-vertical">-->
+<!--                                <h3 class="text-white">Project #10</h3>-->
+<!--                                <a href="#" class="btn btn-primary btn-white">See Project</a>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                    <div class="col-md-4 col-sm-6 project development image-holder">-->
+<!--                        <div class="background-image-holder">-->
+<!--                            <img class="background-image" alt="Background Image" src="img/project_11.jpg">-->
+<!--                        </div>-->
+<!--                        <div class="hover-state">-->
+<!--                            <div class="align-vertical">-->
+<!--                                <h3 class="text-white">Project #11</h3>-->
+<!--                                <a href="#" class="btn btn-primary btn-white">See Project</a>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                    <div class="col-md-4 col-sm-6 project development image-holder">-->
+<!--                        <div class="background-image-holder">-->
+<!--                            <img class="background-image" alt="Background Image" src="img/project_12.jpg">-->
+<!--                        </div>-->
+<!--                        <div class="hover-state">-->
+<!--                            <div class="align-vertical">-->
+<!--                                <h3 class="text-white">Project #12</h3>-->
+<!--                                <a href="#" class="btn btn-primary btn-white">See Project</a>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    </div>-->
+                </div>
+            </div>
+
+        </div>
+
+    </section>
+
+    <section class="pure-text-contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 text-center">
+                    <span class="sub alt-font">Do you like my works?</span>
+                    <h1><span>So let's talk about your project!</span></h1>
+                    <i class="icon icon-chat"></i>
+                </div>
+            </div>
+
+        </div>
+    </section>
+</div>
+
+<?php get_footer(); ?>
 				
